fecfb26b4c93563c9d537eac12a8e934b7bb9a01
Mukhtar Mammadov <161587396+muxtarofficiall@users.noreply.github.com>
2024-05-10 23:05:16 +0400

    Update Task 1.py

--- Task 1.py
+++ Task 1.py
@@ -1,6 +1,6 @@
 class NumberList:
     def __init__(self):
-        self.mylist = []  # Boş bir siyahı yaradırıq
+        self.mylist = []  # Boş siyahı yaradılır
 
     def add_numbers(self, numbers):
         """Rəqəmləri siyahıya əlavə et."""
@@ -8,13 +8,40 @@ class NumberList:
 
     def find_target_sum(self, target):
         """Verilən hədəf üçün iki rəqəmin cəmini tap."""
-        num_index = {}  # Rəqəmləri və indekslərini saxlamaq üçün bir lüğət
+        # Cütləri tapmaq üçün istifadə ediləcək lüğət
+        num_index = {}
 
         # Siyahı üzərində keçərək cütləri tapırıq
         for index, num in enumerate(self.mylist):
-            complement = target - num  # Hədəf üçün lazım olan tamamlayıcı rəqəm
-            if complement in num_index:  # Əgər tamamlayıcı rəqəm artıq mövcuddursa
-                return [num_index[complement], index]  # Tamamlayıcı və cari rəqəmin indekslərini qaytar
-            num_index[num] = index  # Əks təqdirdə cari rəqəmi və indeksini lüğətə əlavə et
+            # Hədəfə çatmaq üçün lazım olan tamamlayıcı rəqəm
+            complement = target - num
 
-        return -1  # Əgər cüt tapılmazsa, -1 qaytar
+            # Əgər tamamlayıcı rəqəm artıq lüğətdə varsa, cüt tapılıb
+            if complement in num_index:
+                return [num_index[complement], index]
+
+            # Əks halda cari rəqəmi lüğətə əlavə edirik
+            num_index[num] = index
+
+        # Əgər heç bir cüt tapılmazsa, -1 qaytarırıq
+        return -1
+
+
+# İstifadə nümunəsi:
+# Boş NumberList obyektini yaradın
+nl = NumberList()
+
+# Siyahıya rəqəmləri əlavə edin
+nl.add_numbers([1, 2, 3, 4, 5])
+
+# İstənilən hədəf üçün cütləri tapın
+target = 7  # Misal üçün 7 hədəfi
+result = nl.find_target_sum(target)  # Bu halda, [1, 4] indekslərini qaytarmalıdır (2 və 5 cütü)
+
+print("Nəticə:", result)  # Çıxış: Nəticə: [1, 4]
+
+# Başqa bir hədəf üçün cütləri yoxlayın
+target = 10  # 10 hədəfi üçün -1 qaytarır
+result = nl.find_target_sum(target)
+
+print("Nəticə:", result)  # Çıxış: Nəticə: -1
